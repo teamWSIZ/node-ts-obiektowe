490ab078e38bce63cad55a91acfca3a8367e535e
Piotr Marecki <pmarecki.pro@gmail.com>
2021-01-16 10:29:35 +0100

    prosty serwer nodejs/express

--- src/express_basics/server.ts
+++ src/express_basics/server.ts
@@ -7,14 +7,26 @@ const port = 3001;
 
 app.s = [];
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send({"comment": 'App works ok!'});
 });
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     let u = new User('kadabra', 112);
     res.send(u);
 });
 
+app.get('/greet', async (req, res) => {
+    console.log('/greet path called');
+    res.send({"comment": "hi there!"});
+});
+
+
+
 console.log(`Starting app; try: http://localhost:${port}/users`);
+
+// let x = {"comment":"App works ok!"}
+// console.log(x['comment'])
+
+
 app.listen(port, '0.0.0.0');
