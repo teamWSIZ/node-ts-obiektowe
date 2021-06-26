869f043f88e234182811639050b1b666186393db
Piotr Marecki <pm@aimachina.io>
2021-06-26 11:35:22 +0200

    lokalizacje

--- src/pg_truskawki/ap.ts
+++ src/pg_truskawki/ap.ts
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    //konfiguracja CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // ustawienie połączenia z bazą
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -20,12 +28,25 @@ app.get('/', async (req, res) => {
     res.send({"comment": 'App works ok!'});
 });
 
+app.get('/gg', async (req, res) => {
+    res.send({"comment": 'Good game!'});
+});
+
+
+
 app.get('/ceny', async (req, res) => {
     let limitstring = req.query.limit;
     if (limitstring===undefined) limitstring='50';
     let limit = parseInt(limitstring);
 
     pool.query('select * from truskawki.cena order by data desc limit $1', [limit], (er, re) => {
+        if (er) throw er;
+        res.send(re.rows);
+    });
+});
+
+app.get('/lokalizacje', async (req, res) => {
+    pool.query('select * from truskawki.location', [], (er, re) => {
         if (er) throw er;
         res.send(re.rows);
     });
@@ -40,19 +61,13 @@ app.get('/cena/add', async (req, res) => {
     console.log(`Zapisuje do lokid=${lokalizacjaid}`);
 
     // ↑↑ to musi być podane z przeglądarki
-    let nu = await pool.query('insert into truskawki.cena(cenakg,lokalizacjaid,data) ' +
-        'VALUES ($1,$2,$3) returning *', [cenakg, lokalizacjaid, data]);
-    res.send(nu.rows[0]);
+    let nowaCena = await pool.query(
+        'insert into truskawki.cena(cenakg,lokalizacjaid,data) VALUES ($1,$2,$3) returning *',
+        [cenakg, lokalizacjaid, data]);
+    res.send(nowaCena.rows[0]);
 });
 
-
-
-
-
 console.log(`Starting app; try: http://localhost:${port}/ceny`);
-
-// let x = {"comment":"App works ok!"}
-// console.log(x['comment'])
 
 
 app.listen(port, '0.0.0.0');
